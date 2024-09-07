2c2e0d2a73039282baeaeca689f554e853017457
Donghyeon Lee <ehdgus001928@gmail.com>
2024-09-07 23:59:11 +0900

    feat: api 인스턴스 수정

--- src/apis/instance.ts
+++ src/apis/instance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const authInstance = axios.create({
-  baseURL: '/api/auth',
+  baseURL: '/auth',
   timeout: 3000,
   headers: {
     'Content-Type': 'application/json',
