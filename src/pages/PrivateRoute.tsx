b37fca9950b26b0bf031cc49aa5db984dd1bf774
Donghyeon Lee <ehdgus001928@gmail.com>
2024-09-20 14:51:27 +0900

    feat: 로그인 리다이렉트 URL 수정

--- src/pages/PrivateRoute.tsx
+++ src/pages/PrivateRoute.tsx
@@ -1,12 +1,22 @@
-import { Navigate, Outlet, useLocation } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
+import { useEffect } from 'react';
 import useAuth from '../hooks/useAuth';
 
 export default function PrivateRoute() {
-  const location = useLocation();
   const { auth } = useAuth();
 
+  useEffect(() => {
+    if (!auth.isLogin) {
+      const loginURL = process.env.NODE_ENV === 'production'
+        ? 'http://34.47.117.26/page/main/login'
+        : 'http://localhost:3000/page/main/login';
+
+      window.location.href = loginURL;
+    }
+  }, [auth.isLogin]);
+
   if (!auth.isLogin) {
-    return <Navigate to="/login" replace state={{ from: location }} />;
+    return null;
   }
 
   return <Outlet />;
